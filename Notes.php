8de7438103665929660066078d1bd0787fe81d71
sumangr8 <30518152+sumangr8@users.noreply.github.com>
2021-01-08 21:35:54 +0530

    Update Notes.php

--- Notes.php
+++ Notes.php
@@ -6,7 +6,14 @@ How to Javascript Variable Use In Php
 $con="<script>document.write(a)</script>";
 echo $con;
 ?>
+============================================
 
-
-
+How to Php variable use In Javascript
+<?php
+$a=10;
+?>
+<script>
+	var b="<?php echo $a ?>";
+	document.write(b);
+</script>
 
